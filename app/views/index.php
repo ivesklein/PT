43aa0ad58bd15bf3f6fc3167a627b036fdc171d5
David Klein <dvdklein@gmail.com>
2015-02-11 15:41:32 -0300

    favicon

--- app/views/index.php
+++ app/views/index.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        
+        <link rel="shortcut icon" href="favicon.png" />
         <title>PT-UAI</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
--- app/views/lti/header.php
+++ app/views/lti/header.php
@@ -2,7 +2,7 @@
 <head>
 	<title>LTI</title>
 
-
+		<link rel="shortcut icon" href="../favicon.png" />
 	    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic" rel="stylesheet" type="text/css">
         <!-- needs images, font... therefore can not be part of ui.css -->
         <link rel="stylesheet" href="../bower_components/font-awesome/css/font-awesome.min.css">
