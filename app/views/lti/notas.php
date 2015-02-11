a7098b938a40649f33b75ace013ab925334b7e97
David Klein <dvdklein@gmail.com>
2015-02-11 16:07:29 -0300

    notas

--- app/views/lti/notas.php
+++ app/views/lti/notas.php
@@ -4,7 +4,32 @@
 <div class="panel panel-default">
         <div class="panel-heading"><strong><span class="glyphicon glyphicon-th"></span> Notas</strong></div>
         <div class="panel-body">
-        	<h3>hola</h3>
+        	<div class="row">
+	        	<div class="panel panel-default col-md-3">
+				        <div class="panel-heading"><strong><span class="glyphicon glyphicon-th"></span> Entrega 1</strong></div>
+				        <div class="panel-body">
+				        	6,1
+				        </div>
+				</div>
+				<div class="panel panel-default col-md-3">
+				        <div class="panel-heading"><strong><span class="glyphicon glyphicon-th"></span> Entrega 2</strong></div>
+				        <div class="panel-body">
+				        	6,1
+				        </div>
+				</div>
+				<div class="panel panel-default col-md-3">
+				        <div class="panel-heading"><strong><span class="glyphicon glyphicon-th"></span> Entrega 3</strong></div>
+				        <div class="panel-body">
+				        	6,1
+				        </div>
+				</div>
+				<div class="panel panel-default col-md-3">
+				        <div class="panel-heading"><strong><span class="glyphicon glyphicon-th"></span> Entrega 4</strong></div>
+				        <div class="panel-body">
+				        	6,1
+				        </div>
+				</div>
+			</div>
         </div>
 </div>
 
