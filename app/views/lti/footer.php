582a4f7f2ae216ae68eae077724e334e970cc843
David Klein <dvdklein@gmail.com>
2015-02-11 15:05:08 -0300

    lti views

--- app/views/lti/footer.php
+++ app/views/lti/footer.php
@@ -1,4 +1,4 @@
-    <script src="scripts/vendor.js"></script>
-    <script src="scripts/ui.js"></script>
+    <script src="../scripts/vendor.js"></script>
+    <script src="../scripts/ui.js"></script>
 </body>
 </html>
--- app/views/lti/header.php
+++ app/views/lti/header.php
@@ -5,10 +5,16 @@
 
 	    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic" rel="stylesheet" type="text/css">
         <!-- needs images, font... therefore can not be part of ui.css -->
-        <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
-        <link rel="stylesheet" href="bower_components/weather-icons/css/weather-icons.min.css">
+        <link rel="stylesheet" href="../bower_components/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="../bower_components/weather-icons/css/weather-icons.min.css">
         <!-- end needs images -->
 
-        <link rel="stylesheet" href="styles/main.css">
+        <link rel="stylesheet" href="../styles/main.css">
+        <style type="text/css">
+        	body{
+        		background-color: white !important;
+        	}
+
+        </style>
 </head>
 <body>
