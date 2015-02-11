786c313a21a1caea7488b70a2c95f511155bb81a
David Klein <dvdklein@gmail.com>
2015-02-11 19:29:07 -0300

    postwc

--- app/controllers/PostWC.php
+++ app/controllers/PostWC.php
@@ -1,5 +1,14 @@
 <?php
 class PostWC {
 	
-	
+	public static function ajxvernota()
+	{
+		$user = Session::get('wc.user' ,"0");
+		if($user!="0"){
+			return $user;//View::make("lti.notas");
+		}else{
+			return "ah?";
+		}
+	}
+
 }
