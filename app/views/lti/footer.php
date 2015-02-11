9723a5bd948b6faa22db0893e052fac7e60fd883
David Klein <dvdklein@gmail.com>
2015-02-11 14:55:43 -0300

    lti views

---
+++ app/views/lti/footer.php
@@ -0,0 +1,4 @@
+    <script src="scripts/vendor.js"></script>
+    <script src="scripts/ui.js"></script>
+</body>
+</html>
---
+++ app/views/lti/notas.php
@@ -0,0 +1,12 @@
+<?php echo View::make('lti.header'); ?>
+
+
+<div class="panel panel-default">
+        <div class="panel-heading"><strong><span class="glyphicon glyphicon-th"></span> Notas</strong></div>
+        <div class="panel-body">
+        	<h3>hola</h3>
+        </div>
+</div>
+
+
+<?php echo View::make('lti.footer'); ?>
