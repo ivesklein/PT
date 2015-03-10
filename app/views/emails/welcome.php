a4c1b843974ed4e31d428b07135e3eeb6b84dbda
David Klein <dvdklein@gmail.com>
2015-03-10 16:07:30 -0300

    vista con datos

--- app/views/emails/welcome.php
+++ app/views/emails/welcome.php
@@ -2,4 +2,7 @@ Para ingresar a la Plataforma ingresar a:
 
  <a href="http://titulacion-fic-uai.cl/#/profile/changepass">http://titulacion-fic-uai.cl/</a>
 
+ Con usuario: <?=$user?>
+ y contraseña: <?=$pass?>
+
  Porfavor cambiar contraseña.
