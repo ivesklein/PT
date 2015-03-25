c827300acc4a8e6ee393147c9aca23d4351e6d56
David Klein <dvdklein@gmail.com>
2015-03-25 17:56:11 -0300

    waiting en config webcursos

--- app/views/views/webcursos/webcursos.php
+++ app/views/views/webcursos/webcursos.php
@@ -2,7 +2,7 @@
     <?php 
         $per = Periodo::active_obj();
         if($per!="false"){
-            if(!$per->wc_course==""){
+            if(!$per->wc_course==""){///////////
                     
                 $temas = Subject::wherePeriodo($per->name)->get();
                 $reg = 0;
@@ -142,7 +142,14 @@
                 <?php
 
             }else{
-                echo "<div class='alert alert-warning'>Aun no se selecciona curso para llevar el proceso</div><div class='col-xs-2'><input class='form-control' type='password' id='passi' placeholder='wc pass'></input></div><div class='btn btn-warning' id='confcourse'>Configurar</div>
+                echo "<div class='alert alert-warning'>Aun no se selecciona curso para llevar el proceso</div>
+                        <div class='form-control'>
+                        <label class='col-xs-2'>".Auth::user()->wc_id."</label>
+                        <div class='col-xs-2'>
+                            <input class='form-control' type='password' id='passi' placeholder='wc pass'></input>
+                        </div>
+                        <div class='btn btn-warning' id='confcourse'>Configurar <i style='display:none' class='fa fa-spin fa-refresh waiting'></i></div>
+                        </div>
                 <div id='mensaje' class='alert alert-danger' style='display:none;'></div>
                 <div id='selectcourse'></div>
                 <div id='btnselectcourse'></div>
@@ -182,9 +189,14 @@ foreach($ltis as $lti){
 
 <script type="text/javascript">
 	
+    ok1=false;
+
 	$('#confcourse').on("click", function() {
 		var res=$('#passi').val();
+        
 		if(res!=null && res!=""){
+            $(".waiting").show();
+            $('#confcourse').addClass("disabled");
              $('#mensaje').hide();
             var datos = {
                 "f":"Webcursos_cursos",
@@ -193,12 +205,22 @@ foreach($ltis as $lti){
             ajx({
                 data:datos,
                 ok:function(data) {
-                	$('#selectcourse').append("<span class='ui-select'><select></select></span>")
-                    for(i in data.data){
-                    	var item = data.data[i];
-                    	$('#selectcourse select').append("<option value='"+item.id+"'>"+item.title+"</option>");
+                    if(ok1==false){
+                    	$('#selectcourse').append("<span class='ui-select'><select></select></span>")
+                        for(i in data.data){
+                        	var item = data.data[i];
+                        	$('#selectcourse select').append("<option value='"+item.id+"'>"+item.title+"</option>");
+                        }
+                        $('#btnselectcourse').append("<div class='btn btn-success sel'>Elegir</div>")
+                        ok1=true;
                     }
-                    $('#btnselectcourse').append("<div class='btn btn-success sel'>Elegir</div>")
+                    $(".waiting").hide();
+                    $('#confcourse').removeClass("disabled");
+                },
+                error:function(data) {
+                    $(".waiting").hide();
+                    $('#confcourse').removeClass("disabled");
+                    alert(data);
                 }
             });
 		}else{
